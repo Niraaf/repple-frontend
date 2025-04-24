7a9e8afda6085299f8f740f231fc565e892e28a1
Farin Soriano <72371352+Niraaf@users.noreply.github.com>
2025-04-23 23:40:21 -0700

    small changes to auth pages

--- src/app/(auth)/login/page.js
+++ src/app/(auth)/login/page.js
@@ -3,7 +3,6 @@ import Login from "@/components/auth/login";
 export default function LoginPage () {
     return (
         <div>
-            <h1>HEY this is login.</h1>
             <Login />
         </div>
     )
--- src/app/(auth)/register/page.js
+++ src/app/(auth)/register/page.js
@@ -3,7 +3,6 @@ import Register from "@/components/auth/register";
 export default function RegisterPage() {
     return (
         <div>
-            <h1>HELLO THERE REGISTER</h1>
             <Register />
         </div>
 
